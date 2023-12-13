1f5734a75904b663c6c4b9da3038754d7ecc5e2e
bilrazor <113417155+bilrazor@users.noreply.github.com>
2023-12-13 09:59:09 +0100

    Añadido Backend

---
+++ BackendNBA_Notes/src/test/java/com/project/BackendNBA_Notes/BackendNbaNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BackendNBA_Notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNbaNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
