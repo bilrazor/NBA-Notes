6c4e73710733d3f8b906f09f4e6fef62ff2c8e31
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2023-12-13 11:04:27 +0100

    RegisterActivity y activity_register.xml

---
+++ app/src/main/java/com/project/nba_notes/RegisterActivity.java
@@ -0,0 +1,83 @@
+package com.project.nba_notes;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+    private Button registerButton;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private RequestQueue queue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        editTextUsername = findViewById(R.id.edit_text_username);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        registerButton = findViewById(R.id.button_create_account);
+
+        queue = Volley.newRequestQueue(this);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterActivity.this,"Nombre: "+editTextUsername.getText().toString(), Toast.LENGTH_LONG).show();
+
+                registerUser();
+            }
+        });
+
+    }
+    private void registerUser(){
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("username", editTextUsername.getText().toString());
+            requestBody.put("password", editTextPassword.getText().toString());
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                Server.name + "/users",
+                requestBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(RegisterActivity.this,"Registro correcto", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.networkResponse == null) {
+                            Toast.makeText(RegisterActivity.this,"La conexión no se ha establecido",Toast.LENGTH_LONG).show();
+                        } else {
+                            int serverCode = error.networkResponse.statusCode;
+                            Toast.makeText(RegisterActivity.this,"El servidor respondió con "+serverCode,Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                }
+
+        );
+        this.queue.add(request);
+    }
+
+}
