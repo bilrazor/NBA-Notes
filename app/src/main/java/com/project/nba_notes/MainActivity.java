44503d90201b28462b55e6253f565d7d49f56348
unknown <ncordeiror22@fpcoruna.afundacion.org>
2023-12-14 13:25:48 +0100

    Creado ProfileFragment y su apertura desde el navigation view

--- app/src/main/java/com/project/nba_notes/MainActivity.java
+++ app/src/main/java/com/project/nba_notes/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Context context = this;
     private MaterialToolbar toolbar;
     private DrawerLayout drawerLayout;
+    private FragmentContainerView fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = findViewById(R.id.main_navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentContainer = findViewById(R.id.main_fragment_container);
+
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .setReorderingAllowed(true)
+//                    .add(R.id.main_fragment_container, ProfileFragment.class, null)
+//                    .commit();
+//        }
     }
 
     //metodo deprecado pero mas simple que la alternativa
@@ -51,15 +64,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        // TODO: 14/12/2023 Funcionalidad del menu  
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
         if (id == R.id.menu_item_start)
-            ;
-        else if (id == R.id.menu_item_profile)
-            ;
-        else if (id == R.id.menu_item_favourite)
-            ;
-        else if (id == R.id.menu_item_order)
-            ;
+            fragmentManager.beginTransaction()
+                    .add(R.id.main_fragment_container, ProfileFragment.class, null)
+                    .commit();
+        else if (id == R.id.menu_item_profile);
+            //transaction.replace(R.id.main_fragment_container, );
+        else if (id == R.id.menu_item_favourite);
+            //transaction.replace(R.id.main_fragment_container, );
+        else if (id == R.id.menu_item_order);
+            //transaction.replace(R.id.main_fragment_container, );
+
+        drawerLayout.closeDrawers();
 
         return false;
     }
---
+++ app/src/main/java/com/project/nba_notes/ProfileFragment.java
@@ -0,0 +1,21 @@
+package com.project.nba_notes;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+    public ProfileFragment(){
+        super(R.layout.fragment_profile);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
