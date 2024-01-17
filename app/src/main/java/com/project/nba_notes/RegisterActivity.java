f2e1212f1276b38259ece899d815d02a0e19f0fd
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2024-01-17 11:12:21 +0100

    Cambios varios en el tema, etc

--- app/src/main/java/com/project/nba_notes/RegisterActivity.java
+++ app/src/main/java/com/project/nba_notes/RegisterActivity.java
@@ -2,9 +2,16 @@ package com.project.nba_notes;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
@@ -25,6 +32,7 @@ import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button registerButton;
+    private Button hasAccountButton;
     private ImageButton showPassword1;
     private ImageButton showPassword2;
     private EditText editTextUsername;
@@ -44,22 +52,107 @@ public class RegisterActivity extends AppCompatActivity {
         showPassword1 = findViewById(R.id.show_password_button1);
         showPassword2 = findViewById(R.id.show_password_button2);
         editTextEmail = findViewById(R.id.edit_text_email);
+        hasAccountButton = findViewById(R.id.has_account_button);
         queue = Volley.newRequestQueue(this);
+
+
+
+
+        Drawable drawable1 = ContextCompat.getDrawable(getApplicationContext(), R.drawable.user_icon);
+        Bitmap bitmap1 = ((BitmapDrawable) drawable1).getBitmap();
+        Drawable newDrawable1 = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap1, 20, 20, true));
+
+// Define el color que se va a usar
+        int color1 = Color.parseColor("#FF7800");
+
+// Aplica el filtro de color
+        newDrawable1.setColorFilter(new PorterDuffColorFilter(color1, PorterDuff.Mode.SRC_IN));
+        editTextUsername.setCompoundDrawablesWithIntrinsicBounds(null, null, newDrawable1, null);
+
+
+
+
+        Drawable drawable2 = ContextCompat.getDrawable(getApplicationContext(), R.drawable.password_icon);
+        Bitmap bitmap2 = ((BitmapDrawable) drawable2).getBitmap();
+        Drawable newDrawable2 = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap2, 22, 22, true));
+
+// Define el color que se va a usar
+        int color2 = Color.parseColor("#FF7800");
+
+// Aplica el filtro de color
+        newDrawable2.setColorFilter(new PorterDuffColorFilter(color2, PorterDuff.Mode.SRC_IN));
+        editTextPassword.setCompoundDrawablesWithIntrinsicBounds(null, null, newDrawable2, null);
+
+
+
+
+        Drawable drawable3 = ContextCompat.getDrawable(getApplicationContext(), R.drawable.email_icon);
+        Bitmap bitmap3 = ((BitmapDrawable) drawable3).getBitmap();
+        Drawable newDrawable3 = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap3, 20, 15, true));
+
+// Define el color que se va a usar
+        int color3 = Color.parseColor("#FF7800");
+
+// Aplica el filtro de color
+        newDrawable3.setColorFilter(new PorterDuffColorFilter(color3, PorterDuff.Mode.SRC_IN));
+        editTextEmail.setCompoundDrawablesWithIntrinsicBounds(null, null, newDrawable3, null);
+
+
+
+        Drawable drawable4 = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ojo_abierto);
+        Bitmap bitmap4 = ((BitmapDrawable) drawable4).getBitmap();
+        Drawable newDrawable4 = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap4, 29, 17, true));
+
+// Define el color que se va a usar
+        int color4 = Color.parseColor("#FF7800");
+
+// Aplica el filtro de color
+        newDrawable4.setColorFilter(new PorterDuffColorFilter(color4, PorterDuff.Mode.SRC_IN));
+
+
+
+        Drawable drawable5 = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ojo_cerrado);
+        Bitmap bitmap5 = ((BitmapDrawable) drawable5).getBitmap();
+        Drawable newDrawable5 = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap5, 30, 30, true));
+
+// Define el color que se va a usar
+        int color5 = Color.parseColor("#FF7800");
+
+// Aplica el filtro de color
+        newDrawable5.setColorFilter(new PorterDuffColorFilter(color5, PorterDuff.Mode.SRC_IN));
+
+
+
+        showPassword1.setImageDrawable(newDrawable4);
+        showPassword2.setImageDrawable(newDrawable4);
+
+
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String password = editTextPassword.getText().toString();
                 String password2 = editTextPassword2.getText().toString();
                 CharSequence email = editTextEmail.getText().toString();
-                if (!password.equals(password2)) {
-                    editTextPassword2.setError("Las contraseñas no coinciden");
-                }if (!esEmailValido(email)) {
-                    editTextEmail.setError("Introduce un email válido");
-                } else {
+
+                if (password.equals(password2) && esEmailValido(email)) {
                     Toast.makeText(RegisterActivity.this,"Registro correcto", Toast.LENGTH_LONG).show();
                     registerUser();
+                } else {
+                    if (!password.equals(password2)) {
+                        editTextPassword2.setError("Las contraseñas no coinciden");
+                    }
+                    if (!esEmailValido(email)) {
+                        editTextEmail.setError("Introduce un email válido");
+                    }
                 }
+            }
+        });
 
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -70,7 +163,7 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!isImagenCambiada) {
                     // Cambia la imagen
-                    showPassword1.setImageResource(R.drawable.ojo_cerrado);
+                    showPassword1.setImageDrawable(newDrawable5);
 
                     Typeface tempTypeface = editTextPassword.getTypeface();
 
@@ -81,7 +174,7 @@ public class RegisterActivity extends AppCompatActivity {
                     editTextPassword.setTypeface(tempTypeface);
                 } else {
                     // Cambia la imagen de nuevo a la original
-                    showPassword1.setImageResource(R.drawable.ojo_abierto);
+                    showPassword1.setImageDrawable(newDrawable4);
 
                     Typeface tempTypeface = editTextPassword.getTypeface();
 
@@ -103,7 +196,7 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!isImagenCambiada) {
                     // Cambia la imagen
-                    showPassword2.setImageResource(R.drawable.ojo_cerrado);
+                    showPassword2.setImageDrawable(newDrawable5);
 
                     Typeface tempTypeface = editTextPassword2.getTypeface();
 
@@ -114,7 +207,7 @@ public class RegisterActivity extends AppCompatActivity {
                     editTextPassword2.setTypeface(tempTypeface);
                 } else {
                     // Cambia la imagen de nuevo a la original
-                    showPassword2.setImageResource(R.drawable.ojo_abierto);
+                    showPassword2.setImageDrawable(newDrawable4);
 
                     Typeface tempTypeface = editTextPassword2.getTypeface();
 
@@ -153,7 +246,7 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         Toast.makeText(RegisterActivity.this,"Registro correcto", Toast.LENGTH_LONG).show();
                         finish();
-                        Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                         startActivity(intent);
                     }
                 },
@@ -161,7 +254,7 @@ public class RegisterActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         if (error.networkResponse == null) {
-                            Toast.makeText(RegisterActivity.this,"La conexión no se ha establecido",Toast.LENGTH_LONG).show();
+                            Toast.makeText(RegisterActivity.this,"Error de conexión",Toast.LENGTH_LONG).show();
                         } else {
                             int serverCode = error.networkResponse.statusCode;
                             Toast.makeText(RegisterActivity.this,"El servidor respondió con "+serverCode,Toast.LENGTH_LONG).show();
