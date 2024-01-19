a59ba4eca9c53b04a6585efcdeba755ab2fbb055
unknown <ncordeiror22@fpcoruna.afundacion.org>
2024-01-19 10:52:44 +0100

    Commit para trabajar en casa

--- app/src/main/java/com/project/nba_notes/MainActivity.java
+++ app/src/main/java/com/project/nba_notes/MainActivity.java
@@ -7,17 +7,16 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SearchView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -26,10 +25,12 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.zip.Inflater;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Context context = this;
@@ -38,14 +39,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private FragmentContainerView fragmentContainer;
     private SearchView searchView;
     private static String searchRequest;
+    private ImageButton themeSwapButton;
+    private ImageButton logoutButton;
+    private List<NotesData> allTheNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchView = findViewById(R.id.search_view);
-        searchView.setMaxWidth(android.R.attr.width);
+        initUI();
+        initToolbar();
+
+        themeSwapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -57,21 +69,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 return false;
             }
         });
-
-        toolbar = findViewById(R.id.main_toolbar);
-        setSupportActionBar(toolbar);
-
-        drawerLayout = findViewById(R.id.main_drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.main_navigation_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        fragmentContainer = findViewById(R.id.main_fragment_container);
 
 //        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction()
@@ -91,23 +88,57 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    private void initUI(){
+        themeSwapButton = findViewById(R.id.theme_swap_button);
+        logoutButton = findViewById(R.id.logout_button);
+        fragmentContainer = findViewById(R.id.main_fragment_container);
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        toolbar = findViewById(R.id.main_toolbar);
+
+        searchView = findViewById(R.id.search_view);
+        searchView.setMaxWidth(android.R.attr.width);
+
+        NavigationView navigationView = findViewById(R.id.main_navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initToolbar(){
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        if (id == R.id.menu_item_start);
-            //transaction.replace(R.id.main_fragment_container, );
+        if (id == R.id.menu_item_start) {
+//            Bundle args = new Bundle();
+//            args.putString("CATEGORY", "todas");
+//
+//            fragmentManager.beginTransaction()
+//                    .add(R.id.main_fragment_container, MainFragment.class, args)
+//                    .commit();
+        }
         else if (id == R.id.menu_item_profile)
             fragmentManager.beginTransaction()
                 .add(R.id.main_fragment_container, ProfileFragment.class, null)
                 .commit();
-        else if (id == R.id.menu_item_favourite);
-            //transaction.replace(R.id.main_fragment_container, );
-        else if (id == R.id.menu_item_order);
-            //transaction.replace(R.id.main_fragment_container, );
-
+        else if (id == R.id.menu_item_favourite){
+//            Bundle args = new Bundle();
+//            args.putString("CATEGORY", "favorite");
+//
+//            fragmentManager.beginTransaction()
+//                    .add(R.id.main_fragment_container, MainFragment.class, args)
+//                    .commit();
+        }
+        else if (id == R.id.menu_item_order) {
+        }
         drawerLayout.closeDrawers();
 
         return false;
@@ -118,29 +149,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void sendFilterQuery(){
-        JSONObject requestBody = new JSONObject();
+        JSONObject jsonObject = new JSONObject();
+        JSONArray requestBody = new JSONArray();
         try{
-            requestBody.put("a", searchRequest);
+            jsonObject.put("title", searchRequest);
+            requestBody.put(1, jsonObject);
         }catch(JSONException e){
             throw new RuntimeException(e);
         }
+        allTheNotes.clear();
 
-        JsonObjectRequest request = new JsonObjectRequest(
+        JsonArrayRequestWithAuthHeader2 request = new JsonArrayRequestWithAuthHeader2(
                 Request.Method.GET,
-                "http://10.0.2.2:8000/api/auth/notes/",
+                "http://10.0.2.2:8000/api/auth/notes",
                 requestBody,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
+                response -> {
+                    for(int i = 0; i < response.length(); i++){
+                        try{
+                            JSONObject note = response.getJSONObject(i);
+                            NotesData data = new NotesData(note);
+                            allTheNotes.add(data);
+                        }catch (JSONException e){}
                     }
+                    //
+                    //
+                    //
                 },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+                error -> {
 
-                    }
-                }
+                },
+                context
         );
     }
 }
