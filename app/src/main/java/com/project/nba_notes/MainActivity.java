2e877b5bc0ab89cc450bd1233d499e1563ea6141
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2024-01-25 14:38:09 +0100

    Cambio sin importancia

--- app/src/main/java/com/project/nba_notes/MainActivity.java
+++ app/src/main/java/com/project/nba_notes/MainActivity.java
@@ -12,14 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 
-// Obtener la fecha actual
-        Date fechaActual = new Date();
-
-// Formatear la fecha actual en el formato deseado
-        String fechaConFormato = sdf.format(fechaActual);
-        // Obtén el FragmentManager
         getSupportFragmentManager().beginTransaction()
                 // Reemplaza el contenedor con el ID 'container' por el CalendarFragment
                 .replace(R.id.container, new CalendarFragment())
